5bfde7cc0702de412fd6695cc17612174013f3af
moorthykathiresan <71226212+moorthykathiresan@users.noreply.github.com>
2020-09-13 20:51:26 -0400

    Update 4_Largest_PalindromeProduct.cs

--- 4_Largest_PalindromeProduct.cs
+++ 4_Largest_PalindromeProduct.cs
@@ -7,6 +7,7 @@ public class Program
   A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
   Find the largest palindrome made from the product of two 3-digit numbers.
+  Ref: https://iq.opengenus.org/largest-palindrome-from-product-of-two-3-digit-numbers/
   */
 	public static void Main()
 	{
@@ -20,18 +21,43 @@ public class Program
 	{
 		long biggestPolyndromeNumber = 0;
 		
-		var  biggestLeftNumber = long.Parse(new StringBuilder().Append('9', maxDigit).ToString());
+		var  biggestNumber = long.Parse(new StringBuilder().Append('9', maxDigit).ToString());
 		// example 99 (left) * i (right side of the product to find be found) 
 		
-		for(var i = biggestLeftNumber; i > 0; i--)
+		for(var i = biggestNumber; i >= 100; i--)
 		{
-			var product = i * biggestLeftNumber;
-			if(IsPolyndrome(product))
+			// Step calculation and optimzed to skip many combo is referenced on below website
+			// Ref: https://iq.opengenus.org/largest-palindrome-from-product-of-two-3-digit-numbers/
+			var step = 1;
+			long j = 1;
+			if(i % 11 == 0)
 			{
-				biggestPolyndromeNumber = product;
+				step = 1;
+				j = i;
+			}
+			else
+			{
+				step = 11;
+				j = i - i % 11;
+			}
+			
+			for(; j >= 100; j = j - step)
+			{
+				var product = i * j;
+				
+				if(IsPolyndrome(product))
+				{
+					biggestPolyndromeNumber = product;
+					break;
+				}
+			}
+			if(biggestPolyndromeNumber != 0)
+			{
 				break;
 			}
+			
 		}
+		
 		return biggestPolyndromeNumber;
 	}
 	
